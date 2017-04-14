1ce8ef9bede5a15d776e8eb9d591d3731b8b61d9
T.Kubota <kubota-t@fc.ritsumei.ac.jp>
2017-04-14 18:53:17 +0900

    modified

--- ex1.py
+++ ex1.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 score = np.random.randn(10000)*10+50
 
 ## normal script
 count = 0
 for point in score:
-    if int(point)+1 > 72: count += 1
+    if point+1 >= 72: count += 1
 print count
 
 ## using numpy 
-print len(np.where(score > 72)[0])
+print len(np.where(score+1>= 72)[0])
